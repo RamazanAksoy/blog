af1ea8c95c741a0e1fa6a770484b8c07082a69ae
ramazan <ramazan.aksoy1907@hotmail.com>
2022-12-27 08:35:12 +0300

    rr

---
+++ lib/shared/service/stroge_service.dart
@@ -0,0 +1,20 @@
+import 'package:blog/model/blog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class FireStoreService extends GetxService {
+  Future<FireStoreService> init() async {
+    return this;
+  }
+
+  FirebaseFirestore? db = FirebaseFirestore.instance;
+
+  saveBlogFireStore(Blog blog) {
+    try {
+      db!.collection("blogs").doc().set(blog.toJson());
+      Get.snackbar("Succes", "The record has been successfully added");
+    } catch (e) {
+      Get.snackbar("Eror", "$e");
+    }
+  }
+}
