e816e0367e44d3c9cf73e9d5884f590125b16b5c
RamazanAksoy <ramazan.aksoy1907@hotmail.com>
2022-12-27 13:31:34 +0300

    rr

--- lib/model/blog.dart
+++ lib/model/blog.dart
@@ -1,16 +1,24 @@
 class Blog {
+  String? id;
   String? title;
   String? comment;
 
-  Blog({this.title, this.comment});
+  Blog({this.id,this.title, this.comment});
+
+  @override
+  String toString() {
+    return 'Blog{id: $id,title: $title, comment: $comment}';
+  }
 
   Blog.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     title = json['title'];
     comment = json['comment'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
     data['title'] = this.title;
     data['comment'] = this.comment;
     return data;
